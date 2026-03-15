a3d32d81234fec86ca9d3027be9ffb713091e576
Sagar Dhawane <de.sdhawane@gmail.com>
2026-03-15 12:11:22 +0000

    Seed Updated

--- analyses/product_data_anlytics.sql
+++ analyses/product_data_anlytics.sql
@@ -1,6 +1,6 @@
-SELECT *
-FROM
-{{ref('bronze_products')}};
+select
+ * 
+ from
+ {{ref('bronze_products')}}
+where title like 'A%'
 
-select * from {{ref('bronze_products')}}
-where title like 's%';
